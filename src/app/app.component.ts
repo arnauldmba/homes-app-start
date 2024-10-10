9d32fbca9a2ce31023b26e450eb134be0067d529
Arnauld-Mba <arnauldmba@gmail.com>
2024-10-10 13:36:22 +0200

    Hinzufügen der Detailseite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,32 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { HousingLocationComponent } from './housing-location/housing-location.component';
+import { RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
+import routes from './routes';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   template: `
     <main>
-      <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
-      </header>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+        </header>
+      </a> 
       <section class="content">
-        <app-home></app-home>
+        <!--<app-home></app-home>-->
+        <router-outlet></router-outlet>
       </section>
     </main>
+    <a [routerLink]="['/details', 1]">Details</a> <!-- Exemple de lien -->
   `,
   styleUrls: ['./app.component.css'],
-  imports: [HomeComponent, HousingLocationComponent], 
+  imports: [
+    HomeComponent, 
+    HousingLocationComponent, 
+    RouterModule], 
 })
 export class AppComponent {
   title = 'homes';
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,12 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { HousingLocation } from '../housinglocation';
 import { CommonModule } from '@angular/common';
+import { RouterModule, RouterLink, RouterOutlet } from '@angular/router';  // Importer RouterLink et RouterOutlet
+
 
 @Component({
   standalone: true,
   selector: 'app-housing-location',
   templateUrl: './housing-location.component.html',
-  styleUrls: ['./housing-location.component.css']
+  styleUrls: ['./housing-location.component.css'],
+  imports: [RouterLink, RouterOutlet]
 })
 export class HousingLocationComponent {
   @Input() housingLocation!: HousingLocation;
