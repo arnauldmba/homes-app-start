a1ac33b067f449058d1b1915749407032e6aa279
Arnauld-Mba <arnauldmba@gmail.com>
2024-10-10 16:35:42 +0200

    Möglichkeit, mithilfe der Suchleiste nach einem Haus zu suchen

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,13 +14,25 @@ import {HousingService} from '../housing.service';
 
 export class HomeComponent {
   readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
-  
-  housingService: HousingService = inject(HousingService);  
+  housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
+
   constructor() {    
     this.housingLocationList = this.housingService.getAllHousingLocations();  
+    this.filteredLocationList = this.housingLocationList;
   }
 
   housingLocationList: HousingLocation[] = [];
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+    );
+  }
   
 }
 
